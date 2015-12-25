14772a2cbdcc17fe6c844091cbb27d28bcca4921
reno- <contact@rubiano.fr>
2015-12-25 20:14:46 +0100

    fix dependancies

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from distutils.core import setup
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup,find_packages
 setup(
   name = 'pyprojekt',
   packages = ['pyprojekt'], 
-  version = '0.1',
+  version = '0.1.1',
   description = 'A projekt management library',
   author = 'Pixel Stereo',
+  packages=find_packages(),
+  include_package_data=True,
   author_email = 'contact@pixelstereo.org',
   url = 'https://github.com/PixelStereo/PyProjekt', 
-  download_url = 'https://github.com/PixelStereo/PyProjekt/tarball/0.1', 
+  download_url = 'https://github.com/PixelStereo/PyProjekt/tarball/0.1.1', 
   keywords = ['modular', 'model', 'parameter' , 'project' , 'projekt'], 
-  classifiers = [],
+  classifiers = [
+    'Development Status :: 1 - draft',
+    'Environment :: Application',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GPL',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    ],
 )
