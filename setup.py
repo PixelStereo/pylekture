84f75e90d524e6b50149449cc67acd4d36e11ca1
reno- <contact@rubiano.fr>
2015-12-25 20:05:52 +0100

    add files for publishing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyprojekt',
+  packages = ['pyprojekt'], 
+  version = '0.1',
+  description = 'A projekt management library',
+  author = 'Pixel Stereo',
+  author_email = 'contact@pixelstereo.org',
+  url = 'https://github.com/PixelStereo/PyProjekt', 
+  download_url = 'https://github.com/PixelStereo/PyProjekt/tarball/0.1', 
+  keywords = ['modular', 'model', 'parameter' , 'project' , 'projekt'], 
+  classifiers = [],
+)
