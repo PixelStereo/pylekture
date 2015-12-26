67b4364a92cfc72461f2814e62885b45a9d63ae5
Renaud <contact@rubiano.fr>
2015-12-26 10:56:52 +0100

    Add dépendances json and OSC

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
   version = '0.1.1',
   description = 'A projekt management library',
   author = 'Pixel Stereo',
-  packages=find_packages(),
-  include_package_data=True,
   author_email = 'contact@pixelstereo.org',
+  install_requires=['json, OSC,  ],
+  zip_safe=False)
   url = 'https://github.com/PixelStereo/PyProjekt', 
   download_url = 'https://github.com/PixelStereo/PyProjekt/tarball/0.1.1', 
   keywords = ['modular', 'model', 'parameter' , 'project' , 'projekt'], 
   classifiers = [
-    'Development Status :: 1 - draft',
+    'Development Status :: 1 - alpha',
     'Environment :: Application',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: GPL',
