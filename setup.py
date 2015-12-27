1f8cadd172d753b4d49cad9598a1e3b644f47267
reno- <contact@rubiano.fr>
2015-12-27 11:34:54 +0100

    FIX classifiers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup,find_packages
+from distutils.core import setup
 setup(
   name = 'pyprojekt',
   packages = ['pyprojekt'], 
@@ -10,17 +10,18 @@ setup(
   author = 'Pixel Stereo',
   author_email = 'contact@pixelstereo.org',
   install_requires=['PyOSC'],
-  zip_safe=False)
-  url = 'https://github.com/PixelStereo/PyProjekt', 
+  zip_safe=False,
+  url='https://github.com/PixelStereo/PyProjekt', 
   download_url = 'https://github.com/PixelStereo/PyProjekt/tarball/0.1.1', 
   keywords = ['modular', 'model', 'parameter' , 'project' , 'projekt'], 
   classifiers = [
-    'Development Status :: 1 - alpha',
-    'Environment :: Application',
+    'Development Status :: 1 - Planning',
+    'Environment :: No Input/Output (Daemon)',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: GPL',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Natural Language :: English',
     'Operating System :: OS Independent',
-    'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
