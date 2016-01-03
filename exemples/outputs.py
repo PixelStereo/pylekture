adb6762c1dd71cb1c5554d74b6061ac90f195661
reno- <contact@rubiano.fr>
2016-01-03 20:50:33 +0100

    update examples with new changes

--- exemples/outputs.py
+++ exemples/outputs.py
@@ -18,7 +18,7 @@ my_project = projekt.new_project()
 my_scenario = my_project.new_scenario()
 
 # create an output
-my_output = my_project.new_output()
+my_output = my_project.new_output('OSC')
 
 # get available protocols for this projects
 print 'protocols available for this project :' , my_project.getprotocols() 
@@ -31,22 +31,25 @@ print my_scenario.output
 print my_scenario.getoutput()
 
 # create another output with another protocol
-second_out = my_project.new_output(protocol='PJLINK')
+second_out = my_project.new_output('PJLINK')
 second_out.name = 'another output'
 second_out.ip = socket.gethostbyname(socket.gethostname())
 second_out.udp = 1234
+
+third_out = my_project.new_output('OSC')
+third_out.udp = 22222
 #get a list of all outputs for this project
-print  'ALL outputs :' , my_project.outputs()
+print len(my_project.outputs()) , 'outputs :' , my_project.outputs()
 #get a list of PJLINK outputs for this project
-print  'PJLINK outputs :' , my_project.outputs('PJLINK')
+print len(my_project.outputs('PJLINK')) , 'PJLINK :' , my_project.outputs('PJLINK')
 #get a list of OSC outputs for this project
-print  'OSC outputs :' , my_project.outputs('PJLINK')
+print  len(my_project.outputs('OSC')) , 'OSC :' , my_project.outputs('OSC')
 print '--------------------------------------------------'
 # iterate outputs
 out_counter = 0
 for output in my_project.outputs():
 	out_counter += 1
-	print 'output n°'+str(out_counter)+' :' , output.name , '/' , output.protocol , '/' , output.ip + ':' + str(output.udp)
+	print 'output n°'+str(out_counter)+' :' , output.name , '/' , output.getprotocol() , '/' , output.ip + ':' + str(output.udp)
 print '--------------------------------------------------'
 # create an event
 my_event = my_scenario.new_event(content=['/zob',232])
@@ -55,25 +58,17 @@ pj_event.output = ['PJLINK',1]
 #play first scenario with default output
 my_scenario.play()
 
-my_project.path = '/Users/reno/Desktop/testt.json'
-my_project.write()
-
-
-quit()
-
-
-
 another_project = projekt.new_project()
 
 #play first scenario with second output
-my_scenario.output = 2
-"""THE PROBLEM CAN BE SEEN HERE. THE OUTPUT OF SCENARIO IS NOT IN THE SCENARIO LIST. WHO CREATES AN OUTPUT WHICH IS NOT IN THE INSTANCES LIST ????????"""
+my_scenario.output = ['OSC' , 2]
 print my_project.outputs().index(my_scenario.getoutput())
-print 'new output' , my_scenario.output , my_scenario.getoutput() , my_project.outputs()
 my_scenario.play()
 
 """when creating a scenario, its default output is 1, the default output of the project
 When creating an event, it doesn't have an output. It use output's scenario. But you can assing an output for an event if you want"""
-my_event.output = 1
+my_event.output = ['OSC',1]
 my_scenario.play()
 
+my_project.path = '/Users/reno/Desktop/testt.json'
+my_project.write()
