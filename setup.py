a8b057f2f0054027669e8c8bb2bb1a1fb0c64951
reno- <contact@rubiano.fr>
2016-01-06 15:25:36 +0100

    fix error when building package on travis from another app

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from distutils.core import setup
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), 'rb') as f:
-    long_description = f.read()
 
 setup(
   name = 'pyprojekt',
