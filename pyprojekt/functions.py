fae05e62bd306f745a77f4d0b9f7238e18c38b97
reno- <contact@rubiano.fr>
2016-01-16 15:28:52 +0100

    clean

--- pyprojekt/functions.py
+++ pyprojekt/functions.py
@@ -39,19 +39,6 @@ def unicode2string_list(data):
         rv.append(item)
     return rv
 
-def unicode2_list(data):
-    """convert unicode to list"""
-    if isinstance(data,list):
-        if len(data) == 1:
-            rv = data[0]
-            rv = checkType(rv)
-        else:
-            rv = []
-            for item in data:
-                item = checkType(item)
-                rv.append(item)
-    return rv
-
 def checkType(data):
     """Transform an unicode into its original type"""
     if isinstance(data, unicode):
@@ -65,8 +52,6 @@ def checkType(data):
         data = float(data)
     elif isInt(data):
         data = int(data)
-    else:
-        print ('no type' , data , type(data))
     return data
 
 def isString(value):
@@ -113,45 +98,3 @@ def isInt(value):
         return True
     except:
         return False
-
-"""a_string = u'popo2'
-a_float = u'122.2'
-an_int = u'122'
-print ('a_string' , checkType(a_string))
-print ('a_float' , checkType(a_float))
-print ('an_int' , checkType(an_int))"""
-
-
-
-""" TODO :: REMOVE THE FOLLWOING FUCNTIONS"""
-"""these funcitons are not used now… these come from my first tests without classes using a dict"""
-
-def listdirectory2(path):  
-    """list a directory"""
-    projects_list = []  
-    for root, dirs, files in os.walk(path):  
-        for i in files:  
-            projects_list.append(os.path.join(root, i))  
-    return projects_list
-
-def dict2string(content):
-    """get scenario info"""
-    c = None
-    for a , b in content.items() : 
-        b = str(b)  .replace(',','').replace('\'','') + '\n'
-        if c :
-            c = c + a + " " + b
-        else :
-            c = a + " " + b
-    return c
-
-def string2dict(content):
-    content = content.split('\n')
-    toto = {}
-    print ('CALLLL string2dict function in lekture main module')
-    print ('content' , content)
-    for scenario in content:
-        scenario = scenario.split(" ",1)
-        toto.setdefault(scenario[0],scenario[1:])
-    toto = lekture.unicode2string_dict(toto)
-    return toto
