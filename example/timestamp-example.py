9466e35761304995100a864e5a633714845df018
reno- <contact@rubiano.fr>
2016-04-05 22:51:35 +0200

    add peddler as submodule

---
+++ example/timestamp-example.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os,sys
+lib_path = os.path.abspath('./../')
+sys.path.append(lib_path)
+
+import datetime
+from pylekture.project import new_project
+
+def timestamp():
+	return datetime.datetime.now()
+
+print(type(timestamp()))
+print(timestamp().year)
+print(timestamp().month)
+print(timestamp().day)
+print(timestamp().hour)
+print(timestamp().minute)
+print(timestamp().second)
+print(timestamp().microsecond)
+
+project = new_project()
+scenario = project.new_scenario()
+print(scenario.name)
