2ce0f283ed7ae807b4cd2a8d10c91a7d6fa2dd6a
reno- <contact@rubiano.fr>
2016-04-12 00:00:42 +0200

    split scenario/event

---
+++ pylekture/event.py
@@ -0,0 +1,191 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The Scenario Class
+A scenario is always in a project and it (may) contains events
+"""
+
+import liblo
+import datetime
+import threading
+from time import sleep
+from pylekture.constants import debug
+from pylekture.functions import checkType
+
+
+class Event(object):
+    """Create an Event
+    an Event is like a step of a Scenario.
+    It could be a delay, a goto value, a random process,
+    a loop process or everything you can imagine """
+    def __init__(self, scenario, content=None, name='', description='', output=None):
+        self.project = scenario.project
+        if description == '':
+            description = "event's description"
+        if name == '':
+            name = 'untitled event'
+        if not content:
+            content = "no content for this event"
+        self.name = name
+        self.scenario = scenario
+        self.description = description
+        self.content = content
+        if not output:
+            self.output = 'parent'        
+
+    @staticmethod
+    def getinstances(scenario):
+        """return a list of events for a given scenario"""
+        return scenario.event_list
+
+
+    class Play(threading.Thread):
+        """docstring for PlayOsc"""
+        def __init__(self, out, event):
+            threading.Thread.__init__(self)
+            self.out = out
+            self.content = event.content
+            self.event = event
+            self.start()
+
+        def join(self):
+            threading.Thread.join(self)
+            if debug:
+                print('event-end: ' + self.event.name + ' in ' + self.name + ' ends - ' + str(datetime.datetime.now()))
+
+        def run(self):
+            """play an OSC event"""
+            if debug:
+                print('event-play: ' + self.event.name + ' in ' + str(threading.current_thread().name) + ' at ' + str(datetime.datetime.now()))
+            out = self.out
+            args = self.content
+            if isinstance(args, list):
+                # address is the first item of the list
+                address = args[0]
+                args = args[1:]
+            else:
+                # this is a adress_only without arguments
+                address = args
+                args = None
+            try:
+                target = liblo.Address(out.ip, int(out.udp))
+                if debug:
+                    print('connect to : ' + out.ip + ':' + str(out.udp))
+            except liblo.AddressError as err:
+                print('liblo.AddressError' + str(err))
+            if isinstance(args, list) and 'ramp' in args:
+                # this is a ramp, make it in a separate thread
+                ramp = self.Ramp(target, address, args)
+                ramp.join()
+            elif isinstance(args, list):
+                msg = liblo.Message(address)
+                for arg in args:
+                    arg = checkType(arg)
+                    msg.add(arg)
+                liblo.send(target, msg)
+            else:
+                msg = liblo.Message(address)
+                if args:
+                    msg.add(args)
+                liblo.send(target, msg)
+
+
+        class Ramp(threading.Thread):
+            """Instanciate a thread for Playing a ramp
+            Allow to do several ramps in a same scenario"""
+            def __init__(self, target, address, args):
+                threading.Thread.__init__(self)
+                self.args = args
+                self.address = address
+                self.target = target
+                self.start()
+
+            def run(self):
+                if debug:
+                    print('ramp starts in ' + self.name + ' at ' + str(datetime.datetime.now()))
+                index = self.args.index('ramp')
+                ramp = self.args[index+1]
+                dest = self.args[index-1]
+                dest = checkType(dest)
+                ramp = checkType(ramp)
+                value = 0
+                delta = dest - value
+                delta = float(delta)
+                step = delta / ramp
+                for millisec in range(ramp):
+                    msg = liblo.Message(self.address)
+                    value += step
+                    sleep(0.00072)
+                    msg.add(value)
+                    liblo.send(self.target, msg)
+
+            def join(self):
+                threading.Thread.join(self)
+                if debug:
+                    print('ramp ends in ' + self.name + ' at ' + str(datetime.datetime.now()))
+
+    class Sleep(threading.Thread):
+        """docstring for Sleep"""
+        def __init__(self, duration):
+            threading.Thread.__init__(self)
+            self.duration = duration
+            self.start()
+
+        def run(self):
+            if debug:
+                print('sleep starts in ' + self.name + ' at ' + str(datetime.datetime.now()))
+            sleep(self.duration)
+
+        def join(self):
+            threading.Thread.join(self)
+            if debug:
+                print('sleep ends in ' + self.name + ' at ' + str(datetime.datetime.now()))
+            
+
+    def play(self):
+        """play an event"""
+        wait = 0
+        if isinstance(self.content, list):
+            if len(self.content) == 1 and str(self.content[0]).isdigit():
+                wait = float(self.content[0])
+        elif isinstance(self.content, int):
+            wait = float(self.content)
+        if wait:
+            wait = wait/1000
+            if debug:
+                if wait <= 1:
+                    print('waiting ' + str(wait) + ' second')
+                else:
+                    print('waiting ' + str(wait) + ' seconds')
+            sleeper = self.Sleep(wait)
+            return sleeper
+        else:
+            out = self.getoutput()
+            if out:
+                if out.getprotocol() == 'OSC':
+                    player = self.Play(out, self)
+                    return player
+                else:
+                    print('ERROR 503 - protocol ' + out.getprotocol() + ' is not yet implemented')
+            else:
+                print('there is no output for this event / scenario')
+
+
+    def getoutput(self):
+        """rerurn the current output for this event.
+        If no output is set for this event,
+        parent scenario output will be used"""
+        if self.output == 'parent':
+            output = self.scenario.output
+        else:
+            output = self.output
+        if output:
+            protocol = output[0]
+            output = output[1] - 1
+            try:
+                output = self.scenario._project.outputs(protocol)[output]
+            except IndexError:
+                print('ERROR in getoutput - please clip the value to existing outputs')
+                return False
+        return output
--- pylekture/scenario.py
+++ pylekture/scenario.py
@@ -1,14 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""implements a Scenario Class that contains events"""
+"""
+The Scenario Class
+A scenario is always in a project and it (may) contains events
+"""
 
 import liblo
+import datetime
 import threading
 from time import sleep
 from pylekture.constants import debug
 from pylekture.functions import checkType
-import datetime
+from pylekture.event import Event
 
 
 class Scenario(object):
@@ -46,8 +50,9 @@ class Scenario(object):
         Play a scenario
         It creates a new object play in a separate thread.
         """
-        p = self.Play(self, index)
-        p.join()
+        player = self.Play(self, index)
+        player.join()
+        return player
 
 
     class Play(threading.Thread):
@@ -63,7 +68,7 @@ class Scenario(object):
         def join(self):
             threading.Thread.join(self)
             if debug:
-                print('scenario-end: ' + self.scenario.name + ' in ' + self.name + ' ends')
+                print('scenario-end: ' + self.scenario.name + ' in ' + self.name + ' ends at ' + str(datetime.datetime.now()))
 
         def run(self):
             """play a scenario from the beginning
@@ -82,12 +87,13 @@ class Scenario(object):
                 # start from the index, we will skip the pre-wait sleep
                 index = self.index
             if debug:
-                dbg = 'scenario-play: {scenario} from index {index} in {thread}'
-                print(dbg.format(scenario=self.scenario.name, index=index, thread=str(threading.current_thread().name)))
+                dbg = 'scenario-play: {scenario} from index {index} in {thread} at {time}'
+                print(dbg.format(scenario=self.scenario.name, index=index, thread=str(threading.current_thread().name), time=str(datetime.datetime.now())))
             for event in self.scenario.events()[index:]:
                 # play each event
                 player = event.play()
                 player.join()
+            return player
             if self.scenario.post_wait:
                 # if there is a wait after the scenario, please wait!!
                 if debug:
@@ -172,180 +178,3 @@ class Scenario(object):
                                          'content':event.content\
                                          }})
         return events
-
-
-class Event(object):
-    """Create an Event
-    an Event is like a step of a Scenario.
-    It could be a delay, a goto value, a random process,
-    a loop process or everything you can imagine """
-    def __init__(self, scenario, content=None, name='', description='', output=None):
-        self.project = scenario.project
-        if description == '':
-            description = "event's description"
-        if name == '':
-            name = 'untitled event'
-        if not content:
-            content = "no content for this event"
-        self.name = name
-        self.scenario = scenario
-        self.description = description
-        self.content = content
-        if not output:
-            self.output = 'parent'        
-
-    @staticmethod
-    def getinstances(scenario):
-        """return a list of events for a given scenario"""
-        return scenario.event_list
-
-
-    class Play(threading.Thread):
-        """docstring for PlayOsc"""
-        def __init__(self, out, event):
-            threading.Thread.__init__(self)
-            self.out = out
-            self.content = event.content
-            self.event = event
-            self.start()
-
-        def join(self):
-            threading.Thread.join(self)
-            if debug:
-                print('event-end: ' + self.event.name + ' in ' + self.name + ' ends')
-
-        def run(self):
-            """play an OSC event"""
-            if debug:
-                print('event-play: ' + self.event.name + ' in ' + str(threading.current_thread().name))
-            out = self.out
-            args = self.content
-            if isinstance(args, list):
-                # address is the first item of the list
-                address = args[0]
-                args = args[1:]
-            else:
-                # this is a adress_only without arguments
-                address = args
-                args = None
-            try:
-                target = liblo.Address(out.ip, int(out.udp))
-                if debug:
-                    print('connect to : ' + out.ip + ':' + str(out.udp))
-            except liblo.AddressError as err:
-                print('liblo.AddressError' + str(err))
-            if isinstance(args, list) and 'ramp' in args:
-                # this is a ramp, make it in a separate thread
-                ramp = self.Ramp(target, address, args)
-                ramp.join()
-            elif isinstance(args, list):
-                msg = liblo.Message(address)
-                for arg in args:
-                    arg = checkType(arg)
-                    msg.add(arg)
-                liblo.send(target, msg)
-            else:
-                msg = liblo.Message(address)
-                if args:
-                    msg.add(args)
-                liblo.send(target, msg)
-
-
-        class Ramp(threading.Thread):
-            """Instanciate a thread for Playing a ramp
-            Allow to do several ramps in a same scenario"""
-            def __init__(self, target, address, args):
-                threading.Thread.__init__(self)
-                self.args = args
-                self.address = address
-                self.target = target
-                self.start()
-
-            def run(self):
-                if debug:
-                    print('ramp ' + self.name + ' start')
-                index = self.args.index('ramp')
-                ramp = self.args[index+1]
-                dest = self.args[index-1]
-                dest = checkType(dest)
-                ramp = checkType(ramp)
-                value = 0
-                delta = dest - value
-                delta = float(delta)
-                step = delta / ramp
-                for millisec in range(ramp):
-                    msg = liblo.Message(self.address)
-                    value += step
-                    sleep(0.0008)
-                    msg.add(value)
-                    liblo.send(self.target, msg)
-
-            def join(self):
-                threading.Thread.join(self)
-                if debug:
-                    print('ramp-end: ' + ' in ' + self.name + ' ends')
-
-    class Sleep(threading.Thread):
-        """docstring for Sleep"""
-        def __init__(self, duration):
-            threading.Thread.__init__(self)
-            self.duration = duration
-            self.start()
-
-        def run(self):
-            if debug:
-                print('sleep starts in ' + self.name)
-            sleep(self.duration)
-
-        def join(self):
-            threading.Thread.join(self)
-            if debug:
-                print('sleep-end: ' + ' in ' + self.name + ' ends')
-            
-
-    def play(self):
-        """play an event"""
-        wait = 0
-        if isinstance(self.content, list):
-            if len(self.content) == 1 and str(self.content[0]).isdigit():
-                wait = float(self.content[0])
-        elif isinstance(self.content, int):
-            wait = float(self.content)
-        if wait:
-            wait = wait/1000
-            if debug:
-                if wait <= 1:
-                    print('waiting ' + str(wait) + ' second')
-                else:
-                    print('waiting ' + str(wait) + ' seconds')
-            sleeper = self.Sleep(wait)
-            return sleeper
-        else:
-            out = self.getoutput()
-            if out:
-                if out.getprotocol() == 'OSC':
-                    player = self.Play(out, self)
-                    return player
-                else:
-                    print('ERROR 503 - protocol ' + out.getprotocol() + ' is not yet implemented')
-            else:
-                print('there is no output for this event / scenario')
-
-
-    def getoutput(self):
-        """rerurn the current output for this event.
-        If no output is set for this event,
-        parent scenario output will be used"""
-        if self.output == 'parent':
-            output = self.scenario.output
-        else:
-            output = self.output
-        if output:
-            protocol = output[0]
-            output = output[1] - 1
-            try:
-                output = self.scenario._project.outputs(protocol)[output]
-            except IndexError:
-                print('ERROR in getoutput - please clip the value to existing outputs')
-                return False
-        return output
