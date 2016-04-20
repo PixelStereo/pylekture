c427061872a03c08bf4447dd029b4b37e400bfbc
reno- <contact@rubiano.fr>
2016-04-20 16:09:14 +0200

    new exception when playing an event and there is no output

--- pylekture/errors.py
+++ pylekture/errors.py
@@ -6,9 +6,18 @@ All the Exceptions declared by the package are here
 ---
 """
 
-class LektureTypeError(LookupError):
-    """docstring for LektureTypeError"""
+class LektureTypeError(Exception):
+    """
+    Waiting for an object of class 'expected', but received an object of class 'received'
+    """
     def __init__(self, expected, received):
         super(LektureTypeError, self).__init__()
         dbg = 'Wait for an {expected} instance object but receive a {received}'
         print(dbg.format(expected=expected, received=received.__class__))
+
+class OutputZeroError(Exception):
+	"""
+	There is no output in this project
+	"""
+	def __init__(self):
+		super(OutputZeroError, self).__init__()
