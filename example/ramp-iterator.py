b956191a8d59ea087452fa374da5a73697754ef6
reno- <contact@rubiano.fr>
2016-04-24 18:18:21 +0200

    work on iterating animations

---
+++ example/ramp-iterator.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+from datetime import datetime
+
+import os,sys
+lib_path = os.path.abspath('./../')
+sys.path.append(lib_path)
+
+from pylekture.project import new_project
+
+p = new_project()
+s = p.new_scenario()
+s.name = 'Scenar avec une ramp $%ù€ù€ÛÁ7å5»[«¶Û»'
+e = p.new_event('Osc', command=['/polo', 1, 'ramp', 1])
+e.name = 'The ramp !!'
+o = p.new_output('OSC')
+
+#e.play()
+
+
+def gen(duration):
+	step = 0.005
+	duration = duration / step
+	duration = int(duration)
+	for step in xrange(duration):
+		debut = datetime.now()
+		sleep(step)
+		fin = datetime.now()
+		print(fin - debut)        
+		yield 'done'
+
+
+
+debut = datetime.now()
+gen(1)
+fin = datetime.now()
+print('-------', fin - debut)
--- ramping.py
+++ ramping.py
@@ -3,13 +3,17 @@ from time import sleep
 from threading import Timer
 
 def print_time():
-	print "From print_time", datetime.now()
+	print('---END---')
 
 def print_some_times():
-    print datetime.now()
-    Timer(5, print_time, ()).start()
-    Timer(10, print_time, ()).start()
-    sleep(11)  # sleep while time-delay events execute
-    print datetime.now()
+	print('---START---')
+	timer = Timer(5, print_time, ())
+	timer.start()
+	timer.join()
+	
 
+debut = datetime.now()
 print_some_times()
+fin = datetime.now()
+delta = fin - debut
+print(delta)
