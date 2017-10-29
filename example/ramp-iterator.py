b75b7c161bb1ce35e41c018b3953084f2c7145df
reno- <contact@rubiano.fr>
2017-10-29 14:43:44 +0100

    revamp Ramp

--- example/ramp-iterator.py
+++ example/ramp-iterator.py
@@ -4,36 +4,9 @@
 from time import sleep
 from datetime import datetime
 
-import os,sys
-lib_path = os.path.abspath('./../')
-sys.path.append(lib_path)
+from pylekture.animations import Ramp
 
-from pylekture.project import new_project
+a = Ramp(origin=0, destination=100, duration=10000, grain=10)
 
-p = new_project()
-s = p.new_scenario()
-s.name = 'Scenar avec une ramp $%ù€ù€ÛÁ7å5»[«¶Û»'
-e = p.new_event('Osc', command=['/polo', 1, 'ramp', 1])
-e.name = 'The ramp !!'
-o = p.new_output('OSC')
-
-#e.play()
-
-
-def gen(duration):
-	step = 0.005
-	duration = duration / step
-	duration = int(duration)
-	for step in xrange(duration):
-		debut = datetime.now()
-		sleep(step)
-		fin = datetime.now()
-		print(fin - debut)        
-		yield 'done'
-
-
-
-debut = datetime.now()
-gen(1)
-fin = datetime.now()
-print('-------', fin - debut)
+print(a)
+a.play()
--- pylekture/animations.py
+++ pylekture/animations.py
@@ -6,26 +6,20 @@ Animation's library
 """
 
 import datetime
-from threading import Timer
+from threading import Timer, Thread, current_thread
 from pylekture.constants import debug
 from pylekture.functions import checkType
-import liblo
 from time import sleep
 from time import time
 
 current_milli_time = lambda: time() * 1000
 
-def bogus():
-    pass
 
-def Ramp(origin=0, destination=1, duration=1000, grain=10):
+def ramp_generator(origin=0, destination=1, duration=1000, grain=10):
     """
-    Instanciate a thread for Playing a ramp
-
+    The Ramp Generator
     step every 10 ms
-
     Allow to do several ramps in a same project / scenario / event
-
     :param target:
     """
     start = current_milli_time()
@@ -37,3 +31,108 @@ def Ramp(origin=0, destination=1, duration=1000, grain=10):
         last = current_milli_time()
         origin += step
         yield origin
+
+
+class Ramp(object):
+    """
+    The Ramp Object
+    a ramp is an interpolation with time as input and a function as output
+    it has
+    - origin (value)
+    - destination (value)
+    - duration (milliseconds)
+    - grain (milliseconds)
+    """
+    def __init__(self, origin=0, destination=1, duration=1000, grain=10, loop=False):
+        super(Ramp, self).__init__()
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
+        self.grain = grain
+        self.loop = loop
+
+    def __repr__(self):
+        s = "Ramp (origin={origin}, destination={destination}, duration={duration}, grain={grain}, loop={loop}"
+        return s.format(origin=self.origin,
+                        destination=self.destination,
+                        duration=self.duration,
+                        grain=self.grain,
+                        loop=self.loop)
+
+    @property
+    def is_template(self):
+        return self._is_template
+    @is_template.setter
+    def is_template(self, state):
+        self._is_template = m_bool(state)
+
+
+    @property
+    def loop(self):
+        """
+        The loop attribute. If true, the loop plays again when it reach its end.
+            :arg: Boolean
+        """
+        return self._loop
+    @loop.setter
+    def loop(self, loop):
+        loop = checkType(loop)
+        if loop == 0:
+            loop = False
+        elif loop > 0:
+            loop = True
+        self._loop = loop
+
+
+    def play(self, output=None):
+        """
+        Play an event
+        It creates a new object play in a separate thread.
+        """
+        self.current_player = Player(self)
+        return self.current_player
+
+    def stop(self):
+        """
+        Stop an event
+        It will destruct the player in the separate thread.
+        """
+        #print(self.current_player)
+        print("stop is not yet implemented")
+
+
+class Player(Thread):
+    """
+    A Player that play things
+    """
+    def __init__(self, ramp):
+        super(Player, self).__init__()
+        self.ramp = ramp
+        print( "thread init")
+        self.start()
+
+    def run(self):
+        player = self.Play(self.ramp)
+        if player:
+            player.join()
+
+    def stop(self):
+        self._stop.set()
+
+
+    class Play(Thread):
+        """
+        Event Player
+        It plays the event in a separate Thread
+        """
+        def __init__(self, ramp):
+            Thread.__init__(self)
+            self.ramp = ramp
+            self.start()
+
+        def run(self):
+            print('---  ' + str(current_thread().name) + ' at ' + str(datetime.datetime.now()))
+            ramper = ramp_generator(self.ramp.origin, self.ramp.destination, self.ramp.duration, self.ramp.grain)
+            for val in ramper:
+                print('---  ' + str(round(val, 2)))
+            print('---  ' + str(current_thread().name) + ' at ' + str(datetime.datetime.now()))
