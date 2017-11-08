b1793326dcd7f17ffc49acc64b665801a7cfca56
reno- <contact@rubiano.fr>
2017-11-08 18:23:58 +0100

    minor

--- examples/01_new_project.py
+++ examples/01_new_project.py
@@ -3,20 +3,5 @@
 
 from pylekture.project import new_project
 
-from pyossia import ossia
-
-my_device = ossia.LocalDevice('PyOssia Test Device')
-my_device.expose(protocol='osc', listening_port=3456, sending_port=5678, logger=True)
-my_int = my_device.add_param('test/numeric/int', value_type='int', default_value=66, domain=[-100, 100], description='an integer')
-my_float = my_device.add_param('test/numeric/float', value_type='float', default_value=0.123456, domain=[-2.1, 2.2])
-
-my_project = new_project(name= 'My Supa Name')
-a_ramp = my_project.new_event('ramp', parameter=my_int, name='event 1', description='my first event', destination=200, durantion=2000)
-a_random = my_project.new_event('random', parameter=my_int, name='event 1', description='event first', destination=100)
-#print(a_random)
-print(a_ramp)
-
-#a_ramp.play()
-
-
-#my_project.play()
+my_project = new_project()
+print(my_project)
--- examples/02_new_scenario.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from pylekture.project import new_project
-
-my_project = new_project()
-print(my_project)
