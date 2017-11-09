42174a5ff7342338c2c9cbb5bc80083cede3994b
reno- <contact@rubiano.fr>
2017-11-09 14:54:42 +0100

    test a ramp editor

---
+++ examples/rampUI.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Example of a ramp UI
+"""
+
+
+
+from pylekture.project import new_project
+
+from pyossia import ossia
+
+my_device = ossia.LocalDevice('PyOssia Test Device')
+my_device.expose(protocol='osc', listening_port=3456, sending_port=5678, logger=False)
+my_int = my_device.add_param('test/numeric/int', value_type='int', default_value=66, domain=[-100, 100], description='an integer')
+my_float = my_device.add_param('test/numeric/float', value_type='float', default_value=0.123456, domain=[-2.1, 2.2])
+
+my_project = new_project(name= 'Demo Project')
+a_ramp = my_project.new_event('ramp', parameter=my_int, name='event 1', description='my first event', destination=200, duration=2000)
+
+
+from pylekture.ramp_UI import Ramp_UI
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    """
+    Main Window Doc String
+    """
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setAutoFillBackground(True)
+        self.rampui = Ramp_UI(a_ramp)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.rampui)
+        main_box = QWidget()
+        main_box.setLayout(self.layout)
+        # assign this device to the mainwindow
+        self.setCentralWidget(main_box)
+        self.move(0, 40)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
---
+++ pylekture/ramp_UI.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Graphical User Interface for Ramp
+
+Make a menu for selecting parameter to animate
+SpinBox for origin, destination and duration
+progressbar
+slider to check
+curve/easing
+
+"""
+
+from PyQt5.QtWidgets import QGroupBox, QSpinBox, QGridLayout, QProgressBar, QPushButton
+
+class Ramp_UI(QGroupBox):
+    """docstring for Ramp_UI"""
+    def __init__(self, ramp):
+        super(Ramp_UI, self).__init__()
+        self.setTitle('Ramp Interface')
+        self.ramp = ramp
+        self.origin = QSpinBox()
+        self.origin.setValue(self.ramp.parameter.value)
+        self.destination = QSpinBox()
+        self.destination.setValue(self.ramp.destination)
+        self.destination.setMaximum(1000)
+        self.duration = QSpinBox()
+        self.duration.setMaximum(100000)
+        self.duration.setValue(self.ramp.duration)
+        self.go = QPushButton()
+        self.go.clicked.connect(self.ramp.play)
+        self.progressbar = QProgressBar()
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.origin, 0, 0)
+        self.layout.addWidget(self.destination, 0, 1)
+        self.layout.addWidget(self.duration, 1, 0)
+        self.layout.addWidget(self.go, 1, 1)
+        self.layout.addWidget(self.progressbar, 2, 0, 2, 2)
+        self.setLayout(self.layout)
