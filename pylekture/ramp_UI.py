4b021eacebbf4cd052356dd3592a188f51ba909b
reno- <contact@rubiano.fr>
2017-11-22 01:36:07 +0100

    Add a slider for displaying ramp

--- pylekture/ramp_UI.py
+++ pylekture/ramp_UI.py
@@ -10,7 +10,8 @@ curve/easing
 
 """
 
-from PyQt5.QtWidgets import QGroupBox, QSpinBox, QGridLayout, QProgressBar, QPushButton
+from PyQt5.QtWidgets import QGroupBox, QSpinBox, QGridLayout, QSlider, QPushButton
+from PyQt5.QtCore import Qt
 
 class Ramp_UI(QGroupBox):
     """docstring for Ramp_UI"""
@@ -37,10 +38,8 @@ class Ramp_UI(QGroupBox):
         self.go = QPushButton()
         self.go.clicked.connect(self.ramp.play)
 
-        self.progressbar = QProgressBar()
-        self.progressbar.setMaximum(2000)
-        self.ramp.started.connect(self.progressbar.reset)
-        #self.ramp.new_val.connect(self.progressbar.setValue)
+        self.progressbar = QSlider(Qt.Horizontal, self)
+        self.progressbar.setMaximum(self.ramp.duration)
         self.ramp.timing.connect(self.timing)
 
         # set the layout
@@ -53,8 +52,7 @@ class Ramp_UI(QGroupBox):
         self.setLayout(self.layout)
 
     def timing(self, val):
-        pass
-        #self.progressbar.setValue(val)
+        self.progressbar.setValue(val)
     
     def origin_update(self, val):
         """
@@ -73,9 +71,54 @@ class Ramp_UI(QGroupBox):
         update duration of the ramp
         """
         self.ramp.duration = val
+        self.progressbar.setMaximum(val)
     
     def parameter_update(self, val):
         """
         update parameter to be ramped
         """
         self.ramp.parameter = val
+
+"""
+random_UI.py file
+"""
+
+class Random_UI(QGroupBox):
+    """docstring for Ramp_UI"""
+    def __init__(self, random):
+        super(Random_UI, self).__init__()
+        self.setTitle('Random Interface')
+        self.random = random
+
+        # create UI objects
+        self.origin = QSpinBox()
+        self.origin.setValue(self.random.parameter.value)
+
+        self.destination = QSpinBox()
+        self.destination.setMaximum(1000)
+        self.destination.setValue(self.random.destination)
+
+        self.duration = QSpinBox()
+        self.duration.setMaximum(100000)
+        self.duration.setValue(self.random.duration)
+
+        self.go = QPushButton()
+        self.go.clicked.connect(self.random.play)
+
+        self.progressbar = QProgressBar()
+        self.progressbar.setMaximum(200)
+        self.random.started.connect(self.progressbar.reset)
+        #self.random.new_val.connect(self.progressbar.setValue)
+        self.random.new_val.connect(self.popo)
+
+        # set the layout
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.origin, 0, 0)
+        self.layout.addWidget(self.destination, 0, 1)
+        self.layout.addWidget(self.duration, 1, 0)
+        self.layout.addWidget(self.go, 1, 1)
+        self.layout.addWidget(self.progressbar, 2, 0, 2, 2)
+        self.setLayout(self.layout)
+
+    def popo(self, val):
+        self.progressbar.setValue(val)
