8c021affee6a91bbb35ef7c026b9052c9044eb6b
reno- <contact@rubiano.fr>
2021-10-19 15:36:17 +0200

    simploify UI

--- pylekture/animation_UI.py
+++ pylekture/animation_UI.py
@@ -10,7 +10,7 @@ curve/easing
 
 """
 
-from PySide6.QtWidgets import QGroupBox, QSpinBox, QGridLayout, QSlider, QPushButton
+from PySide6.QtWidgets import QGroupBox, QSpinBox, QGridLayout, QSlider, QPushButton, QLabel
 from PySide6.QtCore import Qt
 
 
@@ -29,7 +29,7 @@ class Animation_UI(QGroupBox):
         self.origin.valueChanged.connect(self.origin_update)
 
         self.destination = QSpinBox()
-        self.destination.setMaximum(1000)
+        #self.destination.setMaximum(1000)
         self.destination.setValue(self.ramp.destination)
         self.destination.valueChanged.connect(self.destination_update)
 
@@ -46,14 +46,24 @@ class Animation_UI(QGroupBox):
         self.progressbar.setMaximum(self.ramp.duration)
         self.ramp.timing.connect(self.timing)
 
+        self.value = QSlider(Qt.Horizontal, self)
+        self.value.setMaximum(self.ramp.parameter.domain[1])
+        self.ramp.new_val.connect(self.parameter_update)
+
         # set the layout
         self.layout = QGridLayout()
-        self.layout.addWidget(self.origin, 0, 0)
-        self.layout.addWidget(self.destination, 0, 1)
-        self.layout.addWidget(self.duration, 1, 0)
-        self.layout.addWidget(self.go, 1, 1)
-        self.layout.addWidget(self.progressbar, 2, 0, 1, 2)
-        self.setFixedSize(200, 150)
+        self.layout.addWidget(QLabel('origin'), 0, 0, 1, 1)
+        self.layout.addWidget(self.origin, 0, 1, 1, 1)
+        self.layout.addWidget(QLabel('destination'), 0, 2, 1, 1)
+        self.layout.addWidget(self.destination, 0, 3, 1, 1)
+        self.layout.addWidget(QLabel('duration'), 0, 4, 1, 1)
+        self.layout.addWidget(self.duration, 0, 5, 1, 1)        
+        self.layout.addWidget(self.go, 1, 0, 1, 6)
+        self.layout.addWidget(QLabel('timing'), 2, 0, 1, 1)
+        self.layout.addWidget(self.progressbar, 2, 1, 1, 5)
+        self.layout.addWidget(QLabel('value'), 3, 0, 1, 1)
+        self.layout.addWidget(self.value, 3, 1, 1, 5)
+        #self.setFixedSize(400, 150)
         self.setLayout(self.layout)
 
     def timing(self, val):
@@ -82,48 +92,18 @@ class Animation_UI(QGroupBox):
         """
         update parameter to be ramped
         """
-        self.ramp.parameter = val
+        self.ramp.parameter.value = val
 
-"""
-random_UI.py file
-"""
-
-class Random_UI(QGroupBox):
+class Ramp_UI(Animation_UI):
     """docstring for Ramp_UI"""
-    def __init__(self, random):
-        super(Random_UI, self).__init__()
+    def __init__(self, args):
+        super(Ramp_UI, self).__init__(args)
+        self.setTitle('Ramp Interface')
+        self.args = args
+    
+class Random_UI(Animation_UI):
+    """docstring for Ramp_UI"""
+    def __init__(self, args):
+        super(Random_UI, self).__init__(args)
         self.setTitle('Random Interface')
-        self.random = random
-
-        # create UI objects
-        self.origin = QSpinBox()
-        self.origin.setValue(self.random.parameter.value)
-
-        self.destination = QSpinBox()
-        self.destination.setMaximum(1000)
-        self.destination.setValue(self.random.destination)
-
-        self.duration = QSpinBox()
-        self.duration.setMaximum(100000)
-        self.duration.setValue(self.random.duration)
-
-        self.go = QPushButton()
-        self.go.clicked.connect(self.random.play)
-
-        self.progressbar = QProgressBar()
-        self.progressbar.setMaximum(200)
-        self.random.started.connect(self.progressbar.reset)
-        #self.random.new_val.connect(self.progressbar.setValue)
-        self.random.new_val.connect(self.popo)
-
-        # set the layout
-        self.layout = QGridLayout()
-        self.layout.addWidget(self.origin, 0, 0)
-        self.layout.addWidget(self.destination, 0, 1)
-        self.layout.addWidget(self.duration, 1, 0)
-        self.layout.addWidget(self.go, 1, 1)
-        self.layout.addWidget(self.progressbar, 2, 0, 2, 2)
-        self.setLayout(self.layout)
-
-    def popo(self, val):
-        self.progressbar.setValue(val)
+        self.args = args    
